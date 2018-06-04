295b1e340e247f9119dacd48b1055b1c36425e8b
isabelavp <veliz_isabel@hotmail.com>
2018-06-03 23:10:10 -0400

    creando arreglos y practica de grid

---
+++ app.js
@@ -0,0 +1,12 @@
+var dinosaurs = [];
+
+//agregando elementos
+dinosaurs[0] = "Tiranosaurio Rex";
+dinosaurs[1] = "Velociraptor";
+dinosaurs[2] = "Triceratops";
+dinosaurs[3] = "Estegosaurio";
+dinosaurs[4] = "Troodon";
+dinosaurs[5] = "Estiracosaurio";
+
+console.log(dinosaurs);
+
